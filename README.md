14a7702dc9940243c623644fb4fd091edc39dc76
José Sérgio <maverikxd1@gmail.com>
2023-08-02 06:55:27 -0300

    project finished but sill have one bug

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Project-js-tic-tac-toe
-A simple tic tac toe using JS
+# PROJECT TicTacToe
+
+## Description
+
+This project is a TicTacToe game that can be played by two players. The game is played on a 3x3 grid. Each player takes turns marking a square on the grid. The first player to get three squares in a row wins. It was built using JavaScript, HTML, and CSS.
+
+The project if part of the curriculum of [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-tic-tac-toe).
+
+## Preview
+
+<div align="center">
+    
+   <img src="assets/img/preview.png" alt="Preview"/>
+
+</div>
+
+## Live Demo
+
+[Live Demo Link](#)
+
+## Features
+
+- The game can be restarted at any time.
+- Tracks the number of wins for each player.
+- Track Draw games.
+- Tracks the current player's turn.
+-
+
+## Built with
+
+<div align="center">
+
+- ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+- ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+- ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+</div>
+
+## What I Learned
+
+- I learned more about DOM manipulation.
+- I learned how to use modules in JavaScript.
+
+## Bugs
+
+- After the game is restarded, the turn banner doesn't change.
+
+## License
+
+- MIT. See [LICENSE](/LICENSE) for more details.
+- Copyrigth (c) 2023 [José Sérgio](https://github.com/MrEscappe)
--- script/script.js
+++ script/script.js
@@ -1,34 +1,200 @@
+// Selecting elements from the DOM
 const containerChoice = document.querySelector(".container-choice");
 const containerGame = document.querySelector(".container-game");
+const turnText = document.querySelector(".turn");
+const playerxScore = document.querySelector(".playerX_score");
+const playeroScore = document.querySelector(".playerO_score");
+const drawScoreText = document.querySelector(".draw_score");
+const playAgainContainer = document.querySelector(".container-play_again");
+const resetButton = document.querySelector(".reset__button");
+const turnPlayer = document.getElementById("turn-player");
 
 const symbolX = document.querySelector(".cross");
 const symbolO = document.querySelector(".circle");
 
+const cells = document.querySelectorAll(".game_cell");
+
 // Store the choice of the player in a variable
+let player1Choice = "X";
+let player2Choice = "O";
+let player1Turn = true;
+let player1Score = 0;
+let player2Score = 0;
+let drawScore = 0;
+let turnCount = 0;
+let win = false;
 
-let player1Choice = "";
-let player2Choice = "";
-
-symbolX.addEventListener("click", (e) => {
-	player1Choice = "X";
-	player2Choice = "O";
-	console.log(player1Choice);
-	console.log(player2Choice);
+/**
+ * Handles the click event on the symbol buttons.
+ * @param {string} symbol - The symbol chosen by the player.
+ */
+function handleSymbolClick(symbol) {
+	if (symbol === "X") {
+		player1Choice = "X";
+		player2Choice = "O";
+	} else if (symbol === "O") {
+		player1Choice = "X";
+		player2Choice = "O";
+	}
 	changePage();
+}
+
+// Add event listeners to the symbol buttons
+symbolX.addEventListener("click", () => {
+	handleSymbolClick("X");
 });
 
-symbolO.addEventListener("click", (e) => {
-	player1Choice = "O";
-	player2Choice = "X";
-	console.log(player1Choice);
-	console.log(player2Choice);
+symbolO.addEventListener("click", () => {
+	handleSymbolClick("O");
 });
 
-// Change the page to the game page
-
+/**
+ * Changes the page from the choice screen to the game screen.
+ */
 function changePage() {
 	if (player1Choice !== "" && player2Choice !== "") {
 		containerChoice.style.display = "none";
 		containerGame.style.display = "flex";
 	}
 }
+
+// Game
+
+// Array of all possible win combinations
+const WIN_COMBINATIONS = [
+	// Rows
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	// Columns
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	// Diagonal
+	[0, 4, 8],
+	[2, 4, 6],
+];
+
+// Array representing the game board
+let board = ["", "", "", "", "", "", "", "", ""];
+
+// Add click event listeners to all cells
+cells.forEach((cell, index) => {
+	cell.addEventListener("click", () => {
+		const cellContent = cell.querySelector(".game_cell--symbol").textContent;
+		if (cellContent === "" && turnCount < 9 && !win) {
+			if (player1Turn) {
+				cell.querySelector(".game_cell--symbol").textContent = player1Choice;
+				board[index] = player1Choice;
+				updateTurn();
+				player1Turn = false;
+			} else {
+				cell.querySelector(".game_cell--symbol").textContent = player2Choice;
+				board[index] = player2Choice;
+				updateTurn();
+				player1Turn = true;
+			}
+			turnCount++;
+			winCheck();
+		}
+	});
+});
+
+/**
+ * Updates the turn text to show the current player's symbol.
+ */
+function updateTurn() {
+	if (player1Turn) {
+		turnPlayer.textContent = player1Choice;
+	} else {
+		turnPlayer.textContent = player2Choice;
+	}
+}
+
+/**
+ * Checks if a player has won the game.
+ */
+function winCheck() {
+	for (let i = 0; i < WIN_COMBINATIONS.length; i++) {
+		const winCheck = WIN_COMBINATIONS[i];
+		if (board[winCheck[0]] === board[winCheck[1]] && board[winCheck[1]] === board[winCheck[2]] && board[winCheck[0]] !== "") {
+			switch (board[winCheck[0]]) {
+				case "X":
+					turnText.innerHTML = `
+                    <p>${player1Choice} Wins!</p>
+                    `;
+					win = true;
+					player1Score++;
+					playerxScore.textContent = player1Score;
+					playAgainContainer.style.display = "flex";
+					break;
+				case "O":
+					turnText.innerHTML = `
+                    <p>${player2Choice} Wins!</p>
+                    `;
+					win = true;
+					player2Score++;
+					playeroScore.textContent = player2Score;
+					playAgainContainer.style.display = "flex";
+					break;
+			}
+		}
+	}
+	if (turnCount === 9 && !win) {
+		drawScore++;
+		turnText.innerHTML = `
+        <p>Draw!</p>
+        `;
+		drawScoreText.textContent = drawScore;
+		playAgainContainer.style.display = "flex";
+	}
+}
+
+// Add click event listener to the play again button
+playAgainContainer.addEventListener("click", () => {
+	turnCount = 0;
+	win = false;
+	board = ["", "", "", "", "", "", "", "", ""];
+	cells.forEach((cell) => {
+		cell.querySelector(".game_cell--symbol").textContent = "";
+	});
+	playAgainContainer.style.display = "none";
+	player1Turn = true;
+
+	updateTurn(); // Update the turn__player element with the initial value
+
+	turnText.innerHTML = `
+                    <p>
+                        Turn:
+                        <span id="turn-player" class="turn__player">X</span>
+                    </p>
+                `;
+});
+
+// Add click event listener to the reset button
+resetButton.addEventListener("click", () => {
+	turnCount = 0;
+	win = false;
+	board = ["", "", "", "", "", "", "", "", ""];
+	cells.forEach((cell) => {
+		cell.querySelector(".game_cell--symbol").textContent = "";
+	});
+	playAgainContainer.style.display = "none";
+	player1Turn = true;
+
+	updateTurn();
+
+	turnText.innerHTML = `
+                    <p>
+                        Turn:
+                        <span id="turn-player" class="turn__player">X</span>
+                    </p>
+                `;
+
+	player1Score = 0;
+	player2Score = 0;
+	drawScore = 0;
+	playerxScore.textContent = player1Score;
+	playeroScore.textContent = player2Score;
+	drawScoreText.textContent = drawScore;
+});
